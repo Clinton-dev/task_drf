fd8d21ebe551ecc5d2d52f5873ce59879cb0bdda
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-18 10:59:56 +0300

    Remove created field in task model

--- api/models.py
+++ api/models.py
@@ -5,10 +5,6 @@ class Task(models.Model):
     text = models.CharField(max_length=200)
     day = models.CharField(max_length=200)
     reminder = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created']
 
     def __str__(self) -> str:
         return self.text[0:50]
--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from api.models import Task
 class TaskSerializer(serializers.Serializer):
     class Meta:
         model = Task
-        fields =['text','day', 'reminder']
+        fields = '__all__'
