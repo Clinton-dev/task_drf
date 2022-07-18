b9d119b1d3c36a7bd816b32f401ac94a1a045c13
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-18 12:18:36 +0300

    Add api view function

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, delete_task, create_task
 
 urlpatterns = [
-    path('tasks/', task_list )
+    path('tasks/', task_list ),
+    path('tasks/create/', create_task ),
+    path('tasks/delete/<pk>/', delete_task )
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import Response
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Task
 from .serializers import TaskSerializer
 
-@csrf_exempt
+@api_view(['GET'])
 def task_list(request):
     """
     List all tasks
@@ -15,12 +14,14 @@ def task_list(request):
 
 # Todo build: create and delete endpoints
 
+@api_view(['POST'])
 def create_task(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
 
+@api_view(['DELETE'])
 def delete_task(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
