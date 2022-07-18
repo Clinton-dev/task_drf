ff95d8ec6bd4a52457a79d8c868c0bcfb3c12c64
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-18 09:27:26 +0300

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from ast import mod
+from django.db import models
+
+class Task(models.Model):
+    text = models.CharField(max_length=200)
+    day = models.CharField(max_length=200)
+    reminder = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.text[0:50]
